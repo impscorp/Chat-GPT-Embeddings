af94c07acf89471fd2fade5814ad02eff14fc73b
CodeSandbox <support@codesandbox.io>
2023-09-21 06:35:31 +0000

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,15 @@ const HomePage: NextPage = () => {
     <div className="flex flex-col justify-center min-h-screen antialiased text-center text-slate-50">
       <div className="flex flex-col items-center px-6 m-auto mx-auto max-w-7xl">
         <h1 className="max-w-lg text-xl font-extrabold">
-          Domain-specific Chat GPT-3 Starter App
+          Chat GPT with embeddings
         </h1>
         <ul className="mt-10 mb-6 text-xs sm:text-sm text-slate-400">
-          <li><Link href="/embeddings" >Create Embeddings</Link></li>
-          <li><Link href="/docs" >Search</Link></li>
-
+          <li>
+            <Link href="/embeddings">Create Embeddings</Link>
+          </li>
+          <li>
+            <Link href="/docs">Search</Link>
+          </li>
         </ul>
       </div>
     </div>
